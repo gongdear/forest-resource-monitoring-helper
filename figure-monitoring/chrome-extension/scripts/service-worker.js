acc9ac314e763903c31de482b210064f14f244ab
gongdear <gongdear@gmail.com>
2023-01-30 17:23:07 +0800

    feat: update icon status

--- figure-monitoring/chrome-extension/scripts/service-worker.js
+++ figure-monitoring/chrome-extension/scripts/service-worker.js
@@ -1,3 +1,4 @@
+// 浏览器新建tab页事件
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.active && changeInfo.status === 'loading') {
         chrome.action.setIcon({
@@ -13,6 +14,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         });
     }
 });
+// 浏览器切换tab页事件
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.action.setIcon({
+        path: {
+            '16': chrome.runtime.getURL(`icons/disable.png`),
+            '32': chrome.runtime.getURL(`icons/disable.png`),
+            '48': chrome.runtime.getURL(`icons/disable.png`),
+            '128': chrome.runtime.getURL(`icons/disable.png`),
+        },
+    });
+    chrome.action.setPopup({
+        popup: chrome.runtime.getURL(`popups/disable.html`),
+    });
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, callback) {
     const status = request.greet;
     chrome.action.setIcon({
